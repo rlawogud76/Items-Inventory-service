76b8fd8c4d02dff537f5a8ce06bbf38fdb0fb446
rlawogud76 <sundae0609@gmail.com>
2026-01-25 19:15:44 +0900

    Fix: handleGenericPageJump export 누락 수정

--- src/handlers/buttonHandlers/index.js
+++ src/handlers/buttonHandlers/index.js
@@ -4,6 +4,8 @@
 export { 
   handlePageNavigation,
   handlePageJump,
+  handleGenericPageJump,
+  handleGenericPageJumpModal,
   handleRecipeMaterialPageNavigation,
   handleRecipeMaterialStandalonePageNavigation,
   handleRecipeAddPageNavigation
